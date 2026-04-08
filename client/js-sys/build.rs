06a1ad7b7ab8c68be3e19cf2a13d9f3d6a496b84
daxpedda <daxpedda@gmail.com>
2026-04-08 11:38:33 +0200

    `build.rs` Improvements

--- client/js-sys/build.rs
+++ client/js-sys/build.rs
@@ -1,16 +1,19 @@
 //! This file is not shipped to Crates.io, but it is present when depending on
 //! `js-sys` via `git` or `path`.
 
+use std::io::ErrorKind;
 use std::path::Path;
 use std::process::Command;
-use std::{env, fs, process};
+use std::{env, fs, panic, process};
 
 fn main() {
-	if option_env!("JBG_DEV").is_some_and(|value| value == "1")
-		&& option_env!("CI").is_none_or(|value| value != "true")
+	if option_env!("JBG_DEV").is_none_or(|value| value != "1")
+		|| option_env!("CI").is_some_and(|value| value == "true")
 	{
-		search_folder(&env::current_dir().unwrap());
+		return;
+	}
 
+	if search_folder(&env::current_dir().unwrap()) {
 		let status = Command::new("cargo")
 			.env_remove("CARGO_ENCODED_RUSTFLAGS")
 			.current_dir("../../host")
@@ -30,15 +33,33 @@ fn main() {
 	}
 }
 
-fn search_folder(folder: &Path) {
+fn search_folder(folder: &Path) -> bool {
+	let mut any = false;
+
 	for entry in fs::read_dir(folder).unwrap() {
 		let entry = entry.unwrap();
 		let path = entry.path();
 
 		if path.is_file() && path.as_os_str().as_encoded_bytes().ends_with(b".js-sys.rs") {
 			println!("cargo::rerun-if-changed={}", path.display());
+
+			if !any {
+				let js_sys_mtime = fs::metadata(&path).unwrap().modified().unwrap();
+				let r#gen = path.with_extension("").with_extension("gen.rs");
+				let gen_mtime = match fs::metadata(r#gen) {
+					Ok(meta) => Some(meta.modified().unwrap()),
+					Err(error) if error.kind() == ErrorKind::NotFound => None,
+					Err(error) => panic::panic_any(error),
+				};
+
+				if gen_mtime.is_none_or(|gen_mtime| gen_mtime < js_sys_mtime) {
+					any = true;
+				}
+			}
 		} else if path.is_dir() {
-			search_folder(&path);
+			any |= search_folder(&path);
 		}
 	}
+
+	any
 }
--- host/ld/build.rs
+++ host/ld/build.rs
@@ -7,21 +7,21 @@ use std::process::Command;
 use std::{env, fs, panic, process};
 
 fn main() {
-	if option_env!("JBG_DEV").is_some_and(|value| value == "1")
-		&& option_env!("CI").is_none_or(|value| value != "true")
+	if option_env!("JBG_DEV").is_none_or(|value| value != "1")
+		|| option_env!("CI").is_some_and(|value| value == "true")
 	{
-		let any = search_folder(&env::current_dir().unwrap().join("src/js"));
+		return;
+	}
 
-		if any {
-			let status = Command::new("tsc")
-				.current_dir("src/js")
-				.arg("--build")
-				.status()
-				.unwrap();
+	if search_folder(&env::current_dir().unwrap().join("src/js")) {
+		let status = Command::new("tsc")
+			.current_dir("src/js")
+			.arg("--build")
+			.status()
+			.unwrap();
 
-			if !status.success() {
-				process::exit(status.code().unwrap_or(1));
-			}
+		if !status.success() {
+			process::exit(status.code().unwrap_or(1));
 		}
 	}
 }
@@ -40,19 +40,21 @@ fn search_folder(folder: &Path) -> bool {
 		{
 			println!("cargo::rerun-if-changed={}", path.display());
 
-			let ts_mtime = fs::metadata(&path).unwrap().modified().unwrap();
-			let js = path.with_extension("").with_extension("mjs");
-			let js_mtime = match fs::metadata(js) {
-				Ok(meta) => Some(meta.modified().unwrap()),
-				Err(error) if error.kind() == ErrorKind::NotFound => None,
-				Err(error) => panic::panic_any(error),
-			};
+			if !any {
+				let ts_mtime = fs::metadata(&path).unwrap().modified().unwrap();
+				let js = path.with_extension("").with_extension("mjs");
+				let js_mtime = match fs::metadata(js) {
+					Ok(meta) => Some(meta.modified().unwrap()),
+					Err(error) if error.kind() == ErrorKind::NotFound => None,
+					Err(error) => panic::panic_any(error),
+				};
 
-			if js_mtime.is_none_or(|js_mtime| js_mtime < ts_mtime) {
-				any = true;
+				if js_mtime.is_none_or(|js_mtime| js_mtime < ts_mtime) {
+					any = true;
+				}
 			}
 		} else if path.is_dir() {
-			search_folder(&path);
+			any |= search_folder(&path);
 		}
 	}
 
